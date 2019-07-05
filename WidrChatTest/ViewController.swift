778c1dcee7074cd25743e9c0480ccbecae1af641
Dmitry Smetankin <smetankin93@gmail.com>
2019-07-05 15:05:47 +0300

    Initial Commit

---
+++ WidrChatTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WidrChatTest
+//
+//  Created by Smetankin Dmitry on 7/5/19.
+//  Copyright © 2019 Smetankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
